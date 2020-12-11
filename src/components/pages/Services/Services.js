b265bf959d3bdd12928f79d4bf65ce55e86583f7
mukund chamariya <mukund.sc01@gmail.com>
2020-12-11 18:06:35 +0530

    first commit

---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import Pricing from "../../Pricing";
+import { homeObjTwo } from "./Data";
+
+function Services() {
+  return (
+    <>
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Services;
